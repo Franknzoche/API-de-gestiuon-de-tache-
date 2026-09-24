f5fb1e93a905f7e6fd74171f3f2c669b4ecaaae5
frank <fnzoche@gmail.com>
2026-09-24 18:01:21 +0100

    niouveuau

--- src/main/java/com/example/tache/controller/TacheController.java
+++ src/main/java/com/example/tache/controller/TacheController.java
@@ -1,11 +1,14 @@
-package main.java.com.example.tache.controller;
-import java.net.ResponseCache;
-import java.net.Authenticator.RequestorType;
-import java.util.List;
+package com.example.tache.controller;
 
+import java.util.List;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.tache.Entity.Taches;
 import com.example.tache.service.*;
@@ -13,24 +16,21 @@ import com.example.tache.service.*;
 @RestController 
 @RequestMapping("/api/taches")
 public class TacheController {
-    private final TacheService service ;
+    
+    private final TacheService service;
 
     public TacheController(TacheService service) {
         this.service = service;
     }
-
+    
     @PostMapping
     public ResponseEntity<Taches> create(@RequestBody Taches tache){
-        return ResponseEntity.status(HttpStatus.CREATED).Body(service.create(tache));
+        // Correction ici : .body() avec un "b" minuscule
+        return ResponseEntity.status(HttpStatus.CREATED).body(service.create(tache));
     }
+    
     @GetMapping 
-    /*required=false c'est pour ne pas obliger le client a fournir le statut  avant que l'on ne l'affiche la liste de tout les status
-    donc si le client passe un statut ( a faiire , terminee....) on l'affiche cette liste de statut la si non on affiches tous  
-    */
     public List<Taches> ReadTaches(@RequestParam(required=false) Taches.Statut statut){
         return service.Reads(statut);
     }
-    
 }
-
-
